4887774e2fbfe80294a92fe03d08af4fd100ac30
vansten <krzysztof.stengert@hotmail.com>
2013-10-25 13:38:03 +0200

    Scena testowa
    Postawiona scena testowa

---
+++ Game/Assets/Donut/Skrypty/Turlanie.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turlanie : MonoBehaviour {
+
+	private Vector2 wektorRuchu;
+	private Vector3 wektorObrotu;
+	public float KatObrotu = -0.3f;
+	
+	// Use this for initialization
+	void Start () {
+		wektorRuchu.Set(1,0);
+		wektorObrotu.Set(0,0,1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(wektorRuchu);	
+		//transform.Rotate(wektorObrotu, -KatObrotu);
+	}
+}
---
+++ Game/Assets/Kamera/Ruch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ruch : MonoBehaviour {
+	
+	public Transform Donut;
+	public Transform Kamera;
+	private Vector3 wektorPrzesuniecia;
+	
+	// Use this for initialization
+	void Start () {
+		wektorPrzesuniecia.Set (0,0,100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			Kamera.position = Donut.position + wektorPrzesuniecia;
+	}
+}
