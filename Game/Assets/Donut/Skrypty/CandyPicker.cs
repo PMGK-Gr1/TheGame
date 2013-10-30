dcd4d12284a95bd2b4527a863aa60fb0fe61a69c
Roman Chomik <romanchom@gmail.com>
2013-10-30 12:42:12 +0100

    recommit, added missing files

---
+++ Game/Assets/Donut/Skrypty/CandyPicker.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandyPicker : MonoBehaviour {
+
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Candy") {
+			Debug.Log("I Ate Candy");
+			Destroy(other.gameObject);
+		}
+	}
+}
