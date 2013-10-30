c38ff88a500bbf3d3d347e8ecb3bc97e36641672
Roman Chomik <romanchom@gmail.com>
2013-10-30 12:32:28 +0100

    Candy picker added, objects renamed

--- Game/Assets/Donut/Skrypty/Turlanie.cs
+++ Game/Assets/Donut/Skrypty/Turlanie.cs
@@ -17,12 +17,10 @@ public class Turlanie : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		//float force = Input.GetAxis("Horizontal");
-		float force = Input.acceleration.x;
+		float force = Input.acceleration.x + Input.GetAxis("Horizontal");
 		cloth.AddForceAtPosition(new Vector3 (force * forceMultiplier, 0, 0), cylinder.transform.position + new Vector3(0, 0.2f, 0), 0.2f);
-		//this.rigidbody.AddForce(new Vector3 (force * forceMultiplier, 0, 0));
-		//this.rigidbody.AddTorque(new Vector3(0, 0, -force * torqueMultiplier));
 
-		if (Input.touchCount != 0 && cooldown <= 0) {
+		if ((Input.touchCount != 0 || Input.GetKeyDown(KeyCode.Space)) && cooldown <= 0) {
 			cloth.AddForceAtPosition(new Vector3(0, 2000, 0), cylinder.transform.position + new Vector3(0, -0.5f, 0), 0.5f);
 			cloth.AddForceAtPosition(new Vector3(0, 1000, 0), cylinder.transform.position + new Vector3(0, -0.5f, 0), 4f);
 			cooldown = 0.5f;
