5432152cb1f4b6068ddd650dbbd1928ba09b3f34
Elejdor <elejdor@gmail.com>
2013-11-08 00:14:41 +0100

    GameController object is added. It should be responsible for functionalities which are persistent trough the game. I.e. generating level, counting lives and points.

--- Game/Assets/GameController/LevelBuilder.cs
+++ Game/Assets/GameController/LevelBuilder.cs
@@ -5,14 +5,14 @@ public class LevelBuilder : MonoBehaviour {
 
 
     public GameObject LevelPrefab;
+	public GameObject player;
+	
     GameObject current;
     GameObject next;
 
- 
-
+	
 	// Use this for initialization
 	void Start () {
-
         current = Instantiate(LevelPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
         current.layer = 9;
         Vector3 nextVector = new Vector3(current.transform.localScale.x,0,0);
@@ -24,7 +24,7 @@ public class LevelBuilder : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        if (this.transform.position.x > (next.transform.position.x - ((next.transform.localScale.x)/3) ))
+        if (player.transform.position.x > (next.transform.position.x - ((next.transform.localScale.x)/3) ))
         {
             float nextPositionX = next.transform.position.x + next.transform.localScale.x - 0.2f;
             float nextPositionY = next.transform.position.y + Random.Range(-2.0f, 2.0f);
