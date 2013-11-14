8622e5fe086d392a532e0cf45d0a5d19872bd58c
Roman Chomik <romanchom@gmail.com>
2013-11-14 17:56:41 +0100

    Just some more cleaning

--- Game/Assets/Pursuit/Pursuit.cs
+++ Game/Assets/Pursuit/Pursuit.cs
@@ -4,19 +4,21 @@ using System.Collections;
 public class Pursuit : MonoBehaviour {
 
     //public variables
-	public float PursuitSpeed = 30.0f;
+	//public float PursuitSpeed = 30.0f;
 	public float CatchDistance = 60.0f;
 	public float PursuitDelay = 2.0f;
 
     //private variables
 	private float startTime = 0.0f;
 	private RigidDonut donut;
+	private float pursuitSpeed;
 
 
 	// Use this for initialization
 	void Start() {
 		startTime = Time.time + PursuitDelay;
 		donut = RigidDonut.instance;
+		pursuitSpeed = donut.TargetSpeed;
 	}
 
 	// Update is called once per frame
@@ -26,7 +28,7 @@ public class Pursuit : MonoBehaviour {
 			float tmpX = tmpDelta.magnitude / CatchDistance;
 			tmpX -= 2;
 			float currentSpeed = tmpX * tmpX * tmpX * 0.2f + 1;
-			currentSpeed *= PursuitSpeed;
+			currentSpeed *= pursuitSpeed;
 			Vector3 tmpVelocity = currentSpeed * Time.fixedDeltaTime * tmpDelta.normalized;
 
 			if (tmpDelta.magnitude < CatchDistance) {
