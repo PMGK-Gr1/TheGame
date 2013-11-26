d7db8527881d3552ce8eab09c836aed5d7b5c8e9
Roman Chomik <romanchom@gmail.com>
2013-11-26 12:52:47 +0100

    Added billboard obstacle. It's cool.

---
+++ Game/Assets/Level/Scripts/RigidbodySleeper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidbodySleeper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Instance");
+		rigidbody.Sleep();
+		Destroy(this);
+	}
+}
