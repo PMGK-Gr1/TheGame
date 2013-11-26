9f9de7238dd196a267c322cc8237fcd212af3093
Elejdor <elejdor@gmail.com>
2013-11-27 00:13:49 +0100

    Cannon prefab added, script is not finished yet

---
+++ Game/Assets/Level/Scripts/CannonShoot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonShoot : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		RigidDonut donut;
+		if ((donut = other.gameObject.GetComponent<RigidDonut>()) != null) {
+			donut.MilkCannonHit();
+			this.enabled = false;
+		}
+	}
+}
