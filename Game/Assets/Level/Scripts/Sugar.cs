e92411a5c34590aec7b466ca13007b60249b541d
Roman Chomik <romanchom@gmail.com>
2013-11-29 09:11:07 +0100

    Zombie max models killed. Sugar rotation added.

--- Game/Assets/Level/Scripts/Sugar.cs
+++ Game/Assets/Level/Scripts/Sugar.cs
@@ -2,6 +2,21 @@
 using System.Collections;
 
 public class Sugar : MonoBehaviour {
+	private float rotationSpeed = 5;
+	private float rotation;
+	private Vector3 rotationAxis;
+
+	void Start() {
+		rotation = Random.Range(0.0f, 100.0f);
+		rotationAxis = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+	}
+
+
+	void FixedUpdate() {
+		rotation += rotationSpeed;
+		transform.rotation = Quaternion.AngleAxis(rotation, rotationAxis);
+	}
+
 	void OnTriggerEnter(Collider other) {
 		RigidDonut donut;
 		if ((donut = other.gameObject.GetComponent<RigidDonut>()) != null) {
