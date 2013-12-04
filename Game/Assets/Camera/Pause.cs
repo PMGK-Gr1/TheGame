93d551e0d648fcb49e1d78daf9d814b0466a10c4
M124 <m.lewandowski124@gmail.com>
2013-12-04 22:59:10 +0100

    Pause button added.

---
+++ Game/Assets/Camera/Pause.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour
+{
+
+    public Texture PauseButton;
+    public GameObject Light;
+
+    void OnGUI()
+    {
+        Rect button = new Rect(Screen.width * 0.1f, Screen.height * 0.1f, Screen.width * 0.1f, Screen.height * 0.1f);
+        if(GUI.Button(button, PauseButton))
+        {
+            if (Time.timeScale == 0)
+            {
+                Light.GetComponent<Light>().intensity = 0.5f;
+                Time.timeScale = 1;
+            }
+
+            else
+            {
+                Time.timeScale = 0;
+                Light.GetComponent<Light>().intensity = 0.0f;
+            }
+        }
+}
+}
