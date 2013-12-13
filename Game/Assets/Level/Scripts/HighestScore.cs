e58dea5f0531ebd342cd38364eec35ce899621a7
M124 <m.lewandowski124@gmail.com>
2013-12-13 09:29:05 +0100

    Flow&interface

---
+++ Game/Assets/Level/Scripts/HighestScore.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighestScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<TextMesh>().text= "Record: " + PlayerPrefs.GetInt("HighestScore").ToString() + " m";
+        Destroy(this.gameObject, 10.0f);
+    	}
+	
+	
+}
