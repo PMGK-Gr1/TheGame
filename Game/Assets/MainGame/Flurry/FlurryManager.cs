8b185a8107aeffed0cd875d506d276b074eb685e
vansten <krzysztof.stengert@hotmail.com>
2014-01-06 13:36:13 +0100

    Flurry system.

---
+++ Game/Assets/MainGame/Flurry/FlurryManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlurryManager : MonoBehaviour {
+
+	void Awake() {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		FlurryAgent.Instance.onStartSession ("DSYXWKHNM2H2C3QVR9HM");
+		FlurryAgent.Instance.logEvent ("Donut is running");
+		FlurryAgent.Instance.onPageView ();
+	}
+
+	public void OnApplicationPause (bool pause) {
+		if (pause) {
+			FlurryAgent.Instance.onEndSession ();		
+		}
+		else {
+			FlurryAgent.Instance.onStartSession("DSYXWKHNM2H2C3QVR9HM");		
+		}
+	}
+
+	public void OnApplicationQuit() {
+		FlurryAgent.Instance.onEndSession ();
+	}
+
+}
