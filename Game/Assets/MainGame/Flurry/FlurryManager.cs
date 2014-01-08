d324776e9f8fd463c5a638390b1c80e64ef76cf1
vansten <krzysztof.stengert@hotmail.com>
2014-01-08 17:28:58 +0100

    Events fixed (I hope...)

--- Game/Assets/MainGame/Flurry/FlurryManager.cs
+++ Game/Assets/MainGame/Flurry/FlurryManager.cs
@@ -10,20 +10,24 @@ public class FlurryManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		FlurryAgent.Instance.onStartSession ("DSYXWKHNM2H2C3QVR9HM");
+		FlurryAgent.Instance.setLogEnabled (true);
 		FlurryAgent.Instance.logEvent ("Donut is running");
 		FlurryAgent.Instance.onPageView ();
 	}
 
 	public void OnApplicationPause (bool pause) {
 		if (pause) {
+			FlurryAgent.Instance.logEvent ("Application paused");
 			FlurryAgent.Instance.onEndSession ();		
 		}
 		else {
-			FlurryAgent.Instance.onStartSession("DSYXWKHNM2H2C3QVR9HM");		
+			FlurryAgent.Instance.onStartSession("DSYXWKHNM2H2C3QVR9HM");	
+			FlurryAgent.Instance.logEvent ("Application resumed");	
 		}
 	}
 
 	public void OnApplicationQuit() {
+		FlurryAgent.Instance.logEvent ("Application closed");
 		FlurryAgent.Instance.onEndSession ();
 	}
 
