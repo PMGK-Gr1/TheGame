1ea45d4643e22fb9df07d124cfe2bf2a0a485db2
vansten <krzysztof.stengert@hotmail.com>
2014-01-09 22:34:38 +0100

    Last improvements to Flurry system.

--- Game/Assets/MainGame/Flurry/FlurryManager.cs
+++ Game/Assets/MainGame/Flurry/FlurryManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class FlurryManager : MonoSingleton<FlurryManager> {
 
@@ -7,97 +8,139 @@ public class FlurryManager : MonoSingleton<FlurryManager> {
 	private int MoneySpent;
 
 	void OnLevelWasLoaded(int level) {
-#if !UNITY_EDITOR
 		FlurryAgent.Instance.onEndSession ();
-#endif
 	}
 
 	// Use this for initialization
 	void Start () {
 		UpgradesLaunched = 0;
 		Object.DontDestroyOnLoad(gameObject);
-#if !UNITY_EDITOR
 		FlurryAgent.Instance.onStartSession ("DSYXWKHNM2H2C3QVR9HM");
 		FlurryAgent.Instance.setLogEnabled (true);
 		FlurryAgent.Instance.logEvent ("Scene " + Application.loadedLevelName + " loaded");
 		FlurryAgent.Instance.onPageView ();
-#endif
 	}
 
 	public void OnApplicationPause (bool pause) {
-#if !UNITY_EDITOR
 		if (pause) {
 			FlurryAgent.Instance.onEndSession ();		
 		}
 		else {
 			FlurryAgent.Instance.onStartSession("DSYXWKHNM2H2C3QVR9HM");
 		}
-#endif
 	}
 
 	public void OnApplicationQuit() {
-#if !UNITY_EDITOR
 		FlurryAgent.Instance.logEvent ("Application closed");
 		FlurryAgent.Instance.onEndSession ();
-#endif
 	}
 
 	public void SessionLength(float time) {
 		Debug.Log ("Works");
-		FlurryAgent.Instance.logEvent ("Donut was running for: " + time.ToString() + "s");
+		Dictionary<string, string> p = CheckInterval (time, "RunLength");
+		FlurryAgent.Instance.logEvent ("SingleRun", p);
 	}
 
 	public void Distance(float distance) {
 		Debug.Log ("Works");
-		FlurryAgent.Instance.logEvent ("Donut rolled " + distance.ToString () + "m");
+		Dictionary<string, string> p = CheckInterval (distance, "Distance");
+		FlurryAgent.Instance.logEvent ("SingleRun", p);
 	}
 
 	public void CandiesPicked(int candies) {
 		Debug.Log ("Works");
-		FlurryAgent.Instance.logEvent ("Donut picked " + candies.ToString () + " candies in one game");
+		Dictionary<string, string> p = CheckInterval (candies, "CandiesPicked");
+		FlurryAgent.Instance.logEvent ("SingleRun", p);
 	}
 
 	public void BoostNumberPicked (int number) {
 		Debug.Log ("Works");
-		FlurryAgent.Instance.logEvent ("Donut picked " + number.ToString () + " boosts in one game");
+		Dictionary<string, string> p = CheckInterval (number, "BoostNumberPicked");
+		FlurryAgent.Instance.logEvent ("SingleRun", p);
 	}
 
 	public void BoostPicked (string name) {
 		Debug.Log ("Works");
-		FlurryAgent.Instance.logEvent (name + " boost picked");
+		Dictionary<string, string> p = new Dictionary<string, string>();
+		p.Add ("BoostPicked", name);
+		FlurryAgent.Instance.logEvent ("SingleRun", p);
 	}
 
 	public void UpgradeLaunch(string name) {
 		Debug.Log ("Works");
-		FlurryAgent.Instance.logEvent (name + " upgrade launched");
+		Dictionary<string, string> p = new Dictionary<string, string>();
+		p.Add ("UpgradeLaunched", name);
+		FlurryAgent.Instance.logEvent ("SingleRun", p);
 		UpgradesLaunched ++;
 	}
 
 	public void TotalUpgrades () {
 		Debug.Log ("Works");
-		FlurryAgent.Instance.logEvent ("Donut launched " + UpgradesLaunched.ToString() + " upgrades in one game");
+		Dictionary<string, string> p = CheckInterval (UpgradesLaunched, "TotalUpgradesLaunched");
+		FlurryAgent.Instance.logEvent ("SingleRun", p);
 	}
 
 	public void DeathCause (string cause) {
 		Debug.Log ("Works");
-		FlurryAgent.Instance.logEvent ("Donut died because of " + cause);
+		Dictionary<string, string> p = new Dictionary<string, string>();
+		p.Add ("Death", cause);
+		FlurryAgent.Instance.logEvent ("SingleRun", p);
 	}
 
 	public void CandiesSpent () {
 		Debug.Log ("Works");
-		FlurryAgent.Instance.logEvent (MoneySpent.ToString () + " candies was spent in our shop");
+		Dictionary<string, string> p = CheckInterval (MoneySpent, "SugarSpent");
+		FlurryAgent.Instance.logEvent ("SingleShopVisit", p);
 	}
 
 	public void Button (string button) {
 		Debug.Log ("Works");
-#if !UNITY_EDITOR
-		FlurryAgent.Instance.logEvent (button + " button");
-#endif
+		Dictionary<string, string> p = new Dictionary<string, string>();
+		p.Add ("ButtonPressed", button);
+		FlurryAgent.Instance.logEvent ("General", p);
 	}
 
 	public void UpgradeBought (string name, int price) {
 		Debug.Log ("Works");
 		MoneySpent += price;
-		FlurryAgent.Instance.logEvent (name + " upgrade bought");
+		Dictionary<string, string> p = new Dictionary<string, string>();
+		p.Add ("UpgradeBought", name);
+		FlurryAgent.Instance.logEvent ("SingleShopVisit", p);
+	}
+
+	Dictionary<string, string> CheckInterval(float value, string pKey) {
+		Debug.Log (pKey + " " + value);
+		Dictionary<string, string> p = new Dictionary<string, string> ();
+		if (value < 10.0f) {
+			p.Add (pKey, "<10");
+		} 
+		else if (value < 20.0f) {
+			p.Add (pKey, "<20");
+		} 
+		else if (value < 50.0f) {
+			p.Add (pKey, "<50");		
+		}
+		else if (value < 100.0f) {
+			p.Add (pKey, "<100");			
+		} 
+		else if (value < 200.0f) {
+			p.Add (pKey, "<200");			
+		}
+		else if (value < 500.0f) {
+			p.Add (pKey, "<500");			
+		}
+		else if (value < 1000.0f) {
+			p.Add (pKey, "<1000");			
+		}
+		else if (value < 2000.0f) {
+			p.Add (pKey, "<2000");			
+		} 
+		else if (value < 5000.0f) {
+			p.Add (pKey, "<5000");			
+		} 
+		else {
+			p.Add (pKey, ">=5000");			
+		}
+		return p;
 	}
 }
